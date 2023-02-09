03fa4ef190f40b1743015b92ea1f3ec173487d5e
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-02-09 14:15:00 -0500

    feat(users.module.ts): add multer config and controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -6,10 +6,28 @@ import { User } from './users.entity'
 import { UsersService } from './users.service'
 
 import { AccountModule } from 'src/account/account.module'
+import { UsersController } from './users.controller'
+import { MulterModule } from '@nestjs/platform-express/multer'
+import { diskStorage } from 'multer'
 
 @Module({
-  imports: [AccountModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    AccountModule,
+    TypeOrmModule.forFeature([User]),
+    MulterModule.register({
+      dest: './upload',
+      storage: diskStorage({
+        filename(_req, file, callback) {
+          callback(null, Date.now() + '.' + file.originalname.split('.')[1])
+        },
+        destination(_req, _file, callback) {
+          callback(null, './upload')
+        },
+      }),
+    }),
+  ],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
